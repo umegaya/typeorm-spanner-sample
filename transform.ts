6104e1ac9102dd934726857dbb291d04fc821dd0
Takehiro Iyatomi <iyatomi@gmail.com>
2019-10-09 10:57:17 +0900

    update transform test

--- transform.ts
+++ transform.ts
@@ -5,6 +5,7 @@ const parserFactory = require('sql-ddl-to-json-schema');
 
 
 const sqls = [
+	"ALTER TABLE `User` CHANGE `latest_date` `latest_date` int NOT NULL;",
 	"CREATE TABLE `Trend` (" + 
 	"	`id` varchar(255) NOT NULL," + 
 	"	`date` datetime(6) NULL DEFAULT CURRENT_TIMESTAMP(6)," +
@@ -40,9 +41,10 @@ const sqls = [
 ];
 
 const fixtures = [
+    `ALTER TABLE User ALTER COLUMN latest_date int64`,
     [
-        `CREATE TABLE Trend (id string(255) NOT NULL,date timestamp  ,disable string(max) NOT NULL ,manage_user_id string(127) NOT NULL,comment string(255) ,query string(255) ) PRIMARY KEY (id);`, 
-        `{"Trend":{"date":{"type":"default","value":"\\"CURRENT_TIMESTAMP(6)\\""},"disable":{"type":"default","value":"\\"False\\""}}}`,
+        `CREATE TABLE Trend (id string(255) NOT NULL,date timestamp  ,disable string(max) NOT NULL ,disable2 bool NOT NULL ,manage_user_id string(127) NOT NULL,comment string(255) ,query string(255) ) PRIMARY KEY (id);`, 
+        `{"Trend":{"date":{"type":"default","value":"\\"CURRENT_TIMESTAMP(6)\\""},"disable":{"type":"default","value":"\\"False\\""},"disable2":{"type":"default","value":"0"}}}`,
     ],
     [
         `CREATE TABLE User (id int64 NOT NULL ,name string(255) NOT NULL ,name2 string(255) NOT NULL ,data bytes(255) NOT NULL) PRIMARY KEY (id);CREATE INDEX name_idx ON User(name,name2 DESC);CREATE UNIQUE INDEX name2_idx ON User(name2)`,
@@ -54,7 +56,7 @@ const fixtures = [
     `RENAME TABLE User TO User2`,
     `ALTER TABLE User DROP COLUMN latest_date`,
     `ALTER TABLE User ALTER COLUMN latest_date int64`,
-    `ALTER TABLE User ALTER COLUMN latest_date last_date timestamp`,
+    new Error("changing column name latest_date => last_date is not supported"),
     `CREATE INDEX latest_date_and_id ON User(id,latest_date)`,
     `CREATE UNIQUE INDEX User_idx_id_latest_date ON User(id,latest_date)`,
     `CREATE NULL_FILTERED INDEX latest_date_and_id ON User(id,latest_date)`,
@@ -64,27 +66,43 @@ const fixtures = [
 ];
 
 for (const sql of sqls) {
+	console.log(`--------------------- test transform of [${sql}]`)
 	const idx = sqls.indexOf(sql);
 	const parser = (new parserFactory("mysql")).parser;
     parser.feed(sql);
     const schemaSources: SpannerExtendSchemaSources = {};
 	const t = MakeDebug(new SpannerDDLTransformer((v: any) => JSON.stringify(v)));
 	//console.log('parser', parser);
-	const tsql = t.transform(parser.results[0], schemaSources);
-
 	const f = fixtures[idx];
-	let ok = true;
-	if (typeof(f) !== 'string') {
-		ok = (f[0] == tsql.trim() && f[1] == JSON.stringify(schemaSources));
-		console.log(f[0] == tsql.trim(), f[1] == JSON.stringify(schemaSources));
-	} else {
-		ok = f === tsql.trim();
-		if (!ok) {
-			console.log(f, tsql.trim(), f == tsql.trim());
+	try {
+		const tsql = t.transform(parser.results[0], schemaSources);
+
+		let ok = true;
+		if (f instanceof Error) {
+			console.log(`error intended but not happen: ${tsql.trim()}`);
+			process.exit(1);
+		} else if (typeof(f) !== 'string') {
+			ok = (f[0] == tsql.trim() && f[1] == JSON.stringify(schemaSources));
+			console.log(f[0] == tsql.trim(), f[1] == JSON.stringify(schemaSources));
+		} else {
+			ok = f === tsql.trim();
+			if (!ok) {
+				console.log(f, tsql.trim(), f == tsql.trim());
+			}
 		}
-	}
-	if (!ok) {
-		console.log(`sql=[${sql}], transform=[${tsql}], exSchemas=${JSON.stringify(schemaSources)}`);
+		if (!ok) {
+			console.log(`sql=[${sql}], transform=[${tsql}], exSchemas=${JSON.stringify(schemaSources)}`);
+			process.exit(1);
+		}
+	} catch (e) {
+		if (f instanceof Error) {
+			if (f.message.trim() == e.message.trim()) {
+				continue;
+			}
+			console.log(`error intended but message differs: [${f.message.trim()}]/[${e.message.trim()}]`);
+		} else {
+			console.log(`unexpected error: ${e.message}`);
+		}
 		process.exit(1);
 	}
 }
